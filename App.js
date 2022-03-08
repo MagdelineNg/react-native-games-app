bdc6c1c0c4db25b320bd8e136265a6f2cc6e3467
magdeline <magdelinenxl@gmail.com>
2022-03-08 14:50:12 +0800

    Init commit

---
+++ App.js
@@ -0,0 +1,77 @@
+import React, {useState} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import * as Font from 'expo-font';
+import AppLoading from 'expo-app-loading';
+ 
+import Header from './components/Header';
+import StartGameScreen from './screens/StartGameScreen';
+import GameScreen from './screens/GameScreen';
+import GameOverScreen from './screens/GameOverScreen';
+ 
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+  });
+};
+ 
+export default function App() {
+  const [userNumber, setUserNumber] = useState();
+ 
+  const [fontLoaded, setFontLoaded] = useState(false);
+ 
+  const configureNewGameHandler = () => {
+    setGameRounds(0);
+    setUserNumber(null);
+  };
+ 
+  const startGameHandler = (selectedNumber) => {
+    setUserNumber(selectedNumber);
+  };
+ 
+  const [guessRounds, setGameRounds] = useState(0);
+ 
+  const gameOverHandler = (numOfRounds) => {
+    setGameRounds(numOfRounds);
+  };
+ 
+  let content = <StartGameScreen onStartGame={startGameHandler} />;
+ 
+  if (userNumber && guessRounds <= 0) {
+    content = (
+      <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />
+    );
+  } else if (guessRounds > 0) {
+    content = (
+      <GameOverScreen
+        roundsNumber={guessRounds}
+        userNumber={userNumber}
+        onRestart={configureNewGameHandler}
+      />
+    );
+  }
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setFontLoaded(true)}
+        onError={(err) => console.log(err)}
+      />
+    );
+  } else {
+    return (
+      <View style={styles.screen}>
+        <Header title="Guess a Number" />
+        {content}
+      </View>
+    );
+  }
+}
+ 
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
+ 
---
+++ screens/GameScreen.js
@@ -0,0 +1,132 @@
+import React, { useState, useRef, useEffect } from "react";
+import { View, Text, StyleSheet, Alert, ScrollView } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+import NumberContainer from "../components/NumberContainer";
+import Card from "../components/Card";
+import MainButton from "../components/MainButton";
+import DefaultStyles from "../constants/default-styles";
+import BodyText from "../components/BodyText";
+import { Dimensions } from "react-native-web";
+
+const generateRandomBetween = (min, max, exclude) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  const rndNum = Math.floor(Math.random() * (max - min)) + min;
+  if (rndNum === exclude) {
+    return generateRandomBetween(min, max, exclude);
+  } else {
+    return rndNum;
+  }
+};
+
+const renderListItem = (value, numOfRound) => (
+  <View key={value} style={styles.listItem}>
+    <BodyText> #{numOfRound}</BodyText>
+  <BodyText> {value} </BodyText>
+  </View>
+)
+
+const GameScreen = (props) => {
+  const initialGuess = generateRandomBetween(1, 100, props.userChoice);
+  const [currentGuess, setCurrentGuess] = useState(initialGuess); //useState if there is no state (first time generating)
+  //subsequent generateRandomBetween will not be used as state bcos thrs alr a state
+  const [pastGuesses, setPastGuesses] = useState([initialGuess]);
+  const currentLow = useRef(1);
+  const currentHigh = useRef(100);
+
+  const { userChoice, onGameOver } = props;
+
+  useEffect(() => {
+    if (currentGuess === userChoice) {
+      onGameOver(pastGuesses.length);  //pastGuesses is a list
+    }
+  }, [currentGuess, userChoice, onGameOver]);
+
+  const nextGuessHandler = (direction) => {
+    if (
+      (direction === "lower" && currentGuess < props.userChoice) ||
+      (direction === "greater" && currentGuess > props.userChoice)
+    ) {
+      Alert.alert("Don't lie!", "You know that this is wrong...", [
+        { text: "Sorry!", style: "cancel" },
+      ]);
+      return;
+    }
+    if (direction === "lower") {
+      currentHigh.current = currentGuess;
+    } else {
+      currentLow.current = currentGuess + 1;
+    }
+    const nextNumber = generateRandomBetween(
+      currentLow.current,
+      currentHigh.current,
+      currentGuess
+    );
+    setCurrentGuess(nextNumber);
+    //setRounds(curRounds => curRounds + 1);
+    setPastGuesses((curPastGuesses) => [nextNumber, ...curPastGuesses]);
+  };
+
+  return (
+    <View style={styles.screen}>
+      <Text style={DefaultStyles.title}>Opponent's Guess</Text>
+      <NumberContainer>{currentGuess}</NumberContainer>
+      <Card style={styles.buttonContainer}>
+        <MainButton onPress={nextGuessHandler.bind(this, "lower")}>
+          <Ionicons name="md-remove" size={24} color="white" />
+        </MainButton>
+        <MainButton onPress={nextGuessHandler.bind(this, "greater")}>
+          <Ionicons name="md-add" size={24} color="white" />
+        </MainButton>
+      </Card>
+      <View style={styles.listContainer}> 
+
+      <ScrollView contentContainerStyle={styles.list}>
+        {pastGuesses.map((guess, index) => (
+          renderListItem(guess, pastGuesses.length - index)
+        ))}
+      </ScrollView>
+
+      </View>
+
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 10,
+    alignItems: "center",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginTop: Dimensions.get('window').height > 600 ? 20 : 10,
+    width: 400,
+    maxWidth: "90%",
+  },
+  listItem: {
+    borderColor: '#ccc',
+    padding: 15,
+    marginVertical: 10,
+    backgroundColor: 'white',
+    borderWidth: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '60%'
+  },
+  listContainer: {
+    width: '80%',
+    flex: 1
+  },
+  list: {  //flexbox main axis is vertical by default
+    flexGrow: 1,//item added take up as much space but keep other behaviours, more flexible than flex
+    alignItems: 'center', //control content for cross-axis (horizontal)
+    justifyContent: 'flex-end'  //default is flex start --> content start from bottom 
+     //position content along main axis of flexbox
+  }
+});
+
+export default GameScreen;
